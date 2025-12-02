eaf0ab4bce94b4cf4cac71640a8d4b0f9323dce4
muz2020 <103252726+muz2020@users.noreply.github.com>
2025-12-02 10:46:24 +0400

    add the day 3

---
+++ src/app/Copmonets/day3/day3.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day3',
+  imports: [],
+  templateUrl: './day3.html',
+  styleUrl: './day3.css',
+})
+export class Day3 {
+
+}
