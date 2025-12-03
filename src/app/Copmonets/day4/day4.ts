7ef7973b0f3cc6fb180e4a859bedb47a9890d4e1
muz2020 <mohaalialalawi20@gmail.com>
2025-12-03 11:50:59 +0400

    works on it

--- src/app/Copmonets/day4/day4.ts
+++ src/app/Copmonets/day4/day4.ts
@@ -14,8 +14,9 @@ export class Day4 {
   // photo path
   photoPath2: string =
     'https://media.calibraint.com/calibraint-wordpress/wp-content/uploads/2023/07/31125914/Event-Binding-Snippet-Calibraint-1024x465.jpg';
-// photo path
-  photoPath1: string = 'https://png.pngtree.com/png-vector/20231019/ourmid/pngtree-user-profile-avatar-png-image_10211467.png';
+  // photo path
+  photoPath1: string =
+    'https://png.pngtree.com/png-vector/20231019/ourmid/pngtree-user-profile-avatar-png-image_10211467.png';
   // ===== Examples =====
 
   // Interpolation example
@@ -26,8 +27,31 @@ export class Day4 {
   inputValue: string = 'Angular Student';
 
   // Property binding (image)
-  photoAlt: string = 'https://png.pngtree.com/png-vector/20231019/ourmid/pngtree-user-profile-avatar-png-image_10211467.png';
+  photoAlt: string =
+    'https://png.pngtree.com/png-vector/20231019/ourmid/pngtree-user-profile-avatar-png-image_10211467.png';
 
   // Property binding (disabled button)
   isSaving: boolean = true;
+
+  // Event binding methods showing alerts and console logs
+
+  onButtonClick(): void {
+    alert('Button clicked!');
+    console.log('Button clicked!');
+  }
+
+  onInputKeyup(event: KeyboardEvent): void {
+    const input = event.target as HTMLInputElement;
+    console.log('Input value:', input.value);
+  }
+  //no click me method counter
+  counter: number = 0;
+  onClickMe(): void {
+    this.counter++;
+  }
+  // condtions logic button
+  isLoggedIn: boolean = false;
+  toggleLogin(): void {
+    this.isLoggedIn = !this.isLoggedIn;
+  }
 }
