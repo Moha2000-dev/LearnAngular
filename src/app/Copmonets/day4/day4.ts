cbd241e74b250d0615f9b74128ca944b1da8bd41
muz2020 <mohaalialalawi20@gmail.com>
2025-12-03 09:35:34 +0400

    add the assets folders

---
+++ src/app/Copmonets/day4/day4.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-day4',
+  imports: [],
+  templateUrl: './day4.html',
+  styleUrl: './day4.css',
+})
+export class Day4 {
+
+}
